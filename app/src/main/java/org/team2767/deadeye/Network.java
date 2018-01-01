ce061c9bed8742a7baf19c3da301b0d04408ed01
Jeff Hutchison <jeff@jeffhutchison.com>
2018-01-01 08:34:57 -0500

    Check for tethering

--- app/src/main/java/org/team2767/deadeye/Network.java
+++ app/src/main/java/org/team2767/deadeye/Network.java
@@ -1,17 +1,21 @@
 package org.team2767.deadeye;
 
 import org.team2767.deadeye.rx.RxBus;
-import org.team2767.deadeye.rx.RxTether;
 import org.team2767.deadeye.rx.RxUdp;
 
 import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.SocketAddress;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
 import hugo.weaving.DebugLog;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import timber.log.Timber;
@@ -19,14 +23,15 @@ import timber.log.Timber;
 public class Network {
 
     private final static int PORT = 5555;
+    private final static String INTERFACE = "rndis0";
+    private final static String TETHERING = "Tethering";
+    private final static String TETHER_COMMAND = "su -c service call connectivity 30 i32 1";
     private final static String PING = "ping";
     private final static byte[] PONG = "pong".getBytes();
     private final static int PONG_SZ = PONG.length;
     private final static int PONG_LIMIT = 400;
     private final RxBus bus;
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
-    private Disposable disposable;
-    private Observable<DatagramPacket> packets;
 
     @Inject
     public Network(RxBus bus) {
@@ -43,12 +48,36 @@ public class Network {
         return data[0] == (byte) 'p' && data[1] == (byte) 'i';
     }
 
+    private static Iterable<NetworkInterface> networkInterfaces() {
+        try {
+            return Collections.list(NetworkInterface.getNetworkInterfaces());
+        } catch (SocketException e) {
+            Timber.e(e);
+        }
+        return Collections.emptyList();
+    }
+
     @DebugLog
     public void start() {
-        RxTether.startTether().subscribe();
+        Timber.i("Starting Network connections");
+
+        // periodically check for tethering network interface and call tether command if missing
+        Disposable disposable = Observable.interval(5, TimeUnit.SECONDS)
+                .flatMapSingle(i -> Observable.fromIterable(networkInterfaces())
+                        .map(NetworkInterface::getName)
+                        .filter(INTERFACE::equals)
+                        .singleOrError()
+                        .onErrorResumeNext(Single.fromCallable(() -> {
+                            Runtime.getRuntime().exec(TETHER_COMMAND);
+                            return TETHERING;
+                        }))
+                )
+                .filter(TETHERING::equals)
+                .subscribe(Timber::i, Timber::e);
+        compositeDisposable.add(disposable);
 
         // listen for messages on given port
-        packets = RxUdp.observableFrom(PORT).share();
+        Observable<DatagramPacket> packets = RxUdp.observableFrom(PORT).share();
 
         // extract remote address from incoming messages
         Observable<SocketAddress> addresses = packets
@@ -61,18 +90,17 @@ public class Network {
         Observable<DatagramPacket> pongs = addresses.map(a -> new DatagramPacket(PONG, PONG_SZ, a));
 
         // send pong when ping arrives
-        Disposable d = packets
+        disposable = packets
                 .filter(Network::isPing)
                 .withLatestFrom(pongs, (ping, pong) -> pong)
                 .subscribeWith(RxUdp.datagramPacketObserver());
-        compositeDisposable.add(d);
+        compositeDisposable.add(disposable);
     }
 
     @DebugLog
     public void stop() {
         Timber.w("Stopping Network connections");
         compositeDisposable.clear();
-        packets = null;
     }
 
     public enum ConnectionEvent {
